298e8be19ba7bc72322d29d6df7be9dccde126f9
gustavomello32 <141463846+gustavomello32@users.noreply.github.com>
2025-08-21 16:49:42 -0300

    add e modify

--- aula8/formulario.php
+++ aula8/formulario.php
@@ -4,13 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nota Final</title>
+    <style>
+        * {
+            padding: 10px;
+        }
+    </style>
 </head>
-<body>
-    <div>
-    <?php 
-        echo "Teste";
+<body> 
+    <form method="get" action="index.php">
+            <div>
+                Nota 1:
+                <input name="n1" type="number">
+            </div>
+            <div>
+                Nota 2:
+                <input name="n2" type="number">
+            </div>
+            <input type="submit" value="Calcular">
+        </form>
 
-?>
-    </div>
 </body>
 </html>
--- aula8/index.php
+++ aula8/index.php
@@ -4,12 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nota Final</title>
+    <style>
+        .texto {
+            color: darkred;
+            font-weight: bolder;
+            font-size: 20px;
+        }
+    </style>
 </head>
 <body>
     <div>
         <?php 
+        $nota1 = $_GET["n1"];
+        $nota2 = $_GET["n2"];
+        $media = ($nota1 + $nota2) / 2;
 
-
+        if ($media == 10){
+            $sit = "O aluno brocou";
+        }
+        elseif ($media >= 7) {
+            $sit = "o aluno foi: APROVADO"; 
+        }
+        else {
+            $sit = " o aluno foi: <span class = 'texto'>REPROVADO</span>";
+        }
+         echo "A media do aluno entre <span class = 'texto'>$nota1</span> e <span class = 'texto'>$nota2</span> foi: <span class = 'texto'>$media</span> <br>";
+         echo "$sit";  
 ?>
     </div>
 <a href="formulario.php">Voltar</a>  
