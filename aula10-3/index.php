a799eabae7fdc08456b6b77f57ba535b24b3d9ae
gustavomello32 <141463846+gustavomello32@users.noreply.github.com>
2025-08-28 16:28:41 -0300

    add

--- aula10-3/index.php
+++ aula10-3/index.php
@@ -4,11 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contador Personalizado</title>
+    <style>
+        span.numero {
+            margin-right: 10px;
+            font-weight: bold;
+            color: darkblue;
+            display: block;
+        }
+        a {
+            text-decoration: none;
+            color: burlywood;
+            border-radius: 6px;
+        }
+        a:hover {
+            text-decoration: overline;
+            background-color: blue;
+        }
+    </style>
 </head>
 <body>
     <?php
+    $inicio = isset($_GET["v1"])? $_GET["v1"] : 0;
+    $final = isset($_GET["v2"])? $_GET["v2"] : 0;
+    $incremento = isset($_GET["v3"])? $_GET["v3"] : 1;
 
-
+    echo "<h2>Contando de $inicio ate $final, de $incremento em $incremento:</h2>";
+    // se o inicio é menor que o final (contagem crescente)
+    if ($inicio <= $final){
+        for ($i = $inicio; $i <= $final; $i +=  
+       $incremento){
+            echo "<span class = 'numero'>$i</span>";
+        }
+    }
+    // se o inicio é maior que o final (contagem descrescente)
+    else {
+        for($i = $inicio; $i >= $final; $i -= $incremento){
+            echo "<span class = 'numero'>$i</span><br>";
+        }
+    }
 ?>
+<a href="formulario.php">Voltar</a>
 </body>
 </html>
